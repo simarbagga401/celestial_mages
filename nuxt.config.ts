3c31af750895d86979cc97c0271ec09346de1a6f
Simar Bagga <simarbagga401@gmail.com>
2021-11-07 16:15:04 +0530

    feat: set prerender to true in index route

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@ import { defineNuxtConfig } from "nuxt3"
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
+	routes: {
+		"/": { prerender: true },
+	},
 	head: {
 		link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
 	},
